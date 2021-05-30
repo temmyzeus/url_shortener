e494707b86920e36c7550cddcab3f7fc94918436
Awoyele Temiloluwa <awoyeletemiloluwa@gmail.com>
2021-05-30 19:57:34 +0100

    Add Stats Route

--- url_shortener/routes.py
+++ url_shortener/routes.py
@@ -52,3 +52,16 @@ def url_redirect(id):
     else:
         flash('Invalid URL!')
         return redirect(url_for('index'))
+
+@app.route('/stats')
+def stats():
+    conn = get_db_connection()
+
+    db_urls = conn.execute('SELECT id, created, original_url, clicks FROM urls').fetchall()
+
+    urls = []
+    for url in db_urls:
+        url = dict(url)
+        url['short_url'] = request.host_url + hashids.encode(url['id'])
+        urls.append(url)
+    return render_template('stats.html', urls=urls)
